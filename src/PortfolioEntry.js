04b08bd08b6d7e24c41cb472a737cc2a8d772fbf
Legbon <leogbondoc@gmail.com>
2017-05-26 19:09:45 +0800

    Added stuff like collapser and loader. No anims.

--- src/PortfolioEntry.js
+++ src/PortfolioEntry.js
@@ -6,29 +6,33 @@ import { Card, CardImg, CardText, CardBlock,
 
 import moment from 'moment';
 
-class Portfolio extends Component {
+import CollapseText from './CollapseText';
+
+
+class PortfolioEntry extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      collapsed: true
+    };
+  }
   render() {
     const {item} = this.props;
     return (
-    <div>
-      <Card className="PortfolioEntry-Body" block outline color="secondary">
-        <CardBlock>
-          <CardTitle>{item.title}</CardTitle>
-        </CardBlock>
-        <a href={item.live_url}><CardImg top width="100%"  src={item.image_url} alt={item.slug} /></a>
-        <CardBlock>
-          <CardSubtitle>{item.status} | <a className="link" href={item.live_url} target="_new">Live</a>{" | "}<a className="link"  href={item.source_url} target="_new">Source</a></CardSubtitle>
-        </CardBlock>  
-        <CardBlock>
-          <CardSubtitle>{ item.began ? "Began " + moment(item.began).fromNow() : "" } { item.ended ? " - Ended " + moment(item.ended).fromNow() : "" }</CardSubtitle>
-        </CardBlock>
-        <CardBlock>
-          <CardText>{item.description}</CardText>
-        </CardBlock>
-      </Card>
-    </div>
+        <Card className="PortfolioEntry-Body" block outline color="secondary">
+          <CardBlock>
+            <CardTitle>{item.title}</CardTitle>
+          </CardBlock>
+          <a href={item.live_url}><CardImg top width="100%"  src={item.image_url} alt={item.slug} /></a>
+          <CardBlock>
+            <CardSubtitle>{item.status} | <a className="link" href={item.live_url} target="_new">Live</a>{" | "}<a className="link"  href={item.source_url} target="_new">Source</a></CardSubtitle>
+            <p>{ item.began ? "Began " + moment(item.began).fromNow() : "" } { item.ended ? " - Ended " + moment(item.ended).fromNow() : "" }</p>
+
+            <CardText><CollapseText text={item.description} size={100} /></CardText>
+          </CardBlock>
+        </Card>
     );
   }
 }
 
-export default Portfolio;
+export default PortfolioEntry;
